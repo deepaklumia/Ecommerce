7f14867a43d3c3e4a9ec19485e5e02e203c660a2
Deepak Kumar <108293307+deepaklumia@users.noreply.github.com>
2023-02-07 19:19:24 +0000

    chenge in style of home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,20 +17,22 @@ export default function Home() {
   return (
     <>
       <div className='d-flex  flex-wrap mt-3 justify-content-center gap-3 '>
-        {products.map((product) => {
-          return (
-            <div className="card  p-2 text-center box" style={{ width: 330, height: 430 }} key={product.id}>
-              <img src={product.image} className="card-img-top mx-auto d-block " alt="..." style={{ width: 150, height: 200 }} />
-              <div className="card-body">
-                <h5 className="card-title">{(product.title).substring(0, 20)}</h5>
-                <p className="card-text">{product.description.substring(0, 80)}...</p>
-                <p>Price: ${product.price}</p>
-                <a className="btn btn-primary" onClick={() => { dispatch(addToCart(product, "add")) }}>Add to Card</a>
+        {products.length > 0 ? <>
+          {products.map((product) => {
+            return (
+              <div className="card  p-2 text-center box" style={{ width: 330, height: 430 }} key={product.id}>
+                <img src={product.image} className="card-img-top mx-auto d-block " alt="..." style={{ width: 150, height: 200 }} />
+                <div className="card-body">
+                  <h5 className="card-title">{(product.title).substring(0, 20)}</h5>
+                  <p className="card-text">{product.description.substring(0, 80)}...</p>
+                  <p>Price: ${product.price}</p>
+                  <a className="btn btn-primary" onClick={() => { dispatch(addToCart(product, "add")) }}>Add to Card</a>
+                </div>
               </div>
-            </div>
-          )
-        })}
-
+            )
+          })}
+        </>
+          : <h1 className='text-center' style={{ marginTop: 400 }}>Loading....</h1>}
       </div>
     </>
 
